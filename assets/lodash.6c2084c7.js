f1f054559279ca8ccce8bbbdec0f7e0cf464a893
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2023-03-28 13:03:53 +0600

    updated batch exam

--- assets/lodash.6c2084c7.js
+++ assets/lodash.6c2084c7.js
@@ -1,4 +1,4 @@
-import{N as jt}from"./index.67126299.js";var Ri={exports:{}};/**
+import{N as jt}from"./index.21a6cbfb.js";var Ri={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
